6b6901782b8e7f650c56707d3b821935f3081fbe
hashd <kd@danduprolu.com>
2015-09-06 15:15:17 +0530

    Add creational test cases for bitmap

--- test/bitmap_test.exs
+++ test/bitmap_test.exs
@@ -1,8 +1,28 @@
 defmodule BitmapTest do
   use ExUnit.Case
+
   doctest Bitmap
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "create bitmaps with size" do
+    assert Bitmap.new(0) == <<>>
+    assert Bitmap.new(25) == <<0, 0, 0, 0::size(1)>>
+    assert Bitmap.new(32) == <<0, 0, 0, 0>>
+    assert Bitmap.new(120) == <<0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0>>
+  end
+
+  test "create bitmaps with range" do
+    assert Bitmap.new(0..0) == <<0::size(1)>>
+    assert Bitmap.new(0..10) == <<0, 0::size(3)>>
+    assert Bitmap.new(512..591) == <<0, 0, 0, 0, 0, 0, 0, 0, 0, 0>>
+  end
+
+  test "create bitmaps with list" do
+    small_list = List.duplicate(5, 25)
+    big_list = List.duplicate(10, 160)
+
+    assert Bitmap.new([]) == <<>>
+    assert Bitmap.new(small_list) == <<0, 0, 0, 0::size(1)>>
+    assert Bitmap.new(big_list) == <<0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                     0, 0, 0, 0, 0>>
   end
 end
