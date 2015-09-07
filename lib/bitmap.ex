cb57691d3e123fb0aa7700771fb1eca65ff561f2
hashd <kd@danduprolu.com>
2015-09-08 02:08:47 +0530

    Defines new type called argtype which is the type for new/1 arguments

--- lib/bitmap.ex
+++ lib/bitmap.ex
@@ -12,20 +12,21 @@ defmodule Bitmap do
 
   @type bitmap :: binary
   @type index  :: non_neg_integer
-  @type bit    :: 1 | 0 
+  @type bit    :: 1 | 0
+  @type argt   :: non_neg_integer | list | Range.t 
 
-  defcallback new(non_neg_integer | list | Range.t) :: any
-  defcallback at(bitmap, index)                     :: bit
-  defcallback set?(bitmap, index)                   :: boolean
-  defcallback set(bitmap, index)                    :: bitmap
-  defcallback set_all(bitmap)                       :: bitmap
-  defcallback unset?(bitmap, index)                 :: boolean
-  defcallback unset(bitmap, index)                  :: bitmap
-  defcallback unset_all(bitmap)                     :: bitmap
-  defcallback toggle(bitmap, index)                 :: bitmap
-  defcallback toggle_all(bitmap)                    :: bitmap
-  defcallback to_string(bitmap)                     :: String.t
-  defcallback inspect(bitmap)                       :: String.t
+  defcallback new(argt)             :: any
+  defcallback at(bitmap, index)     :: bit
+  defcallback set?(bitmap, index)   :: boolean
+  defcallback set(bitmap, index)    :: bitmap
+  defcallback set_all(bitmap)       :: bitmap
+  defcallback unset?(bitmap, index) :: boolean
+  defcallback unset(bitmap, index)  :: bitmap
+  defcallback unset_all(bitmap)     :: bitmap
+  defcallback toggle(bitmap, index) :: bitmap
+  defcallback toggle_all(bitmap)    :: bitmap
+  defcallback to_string(bitmap)     :: String.t
+  defcallback inspect(bitmap)       :: String.t
 
   defdelegate [new(argument), at(bitmap, index), set?(bitmap, index), 
     set(bitmap, index), set_all(bitmap), unset?(bitmap, index), 
